cf8b98d195cb19c6c8f695bc22a18deeb2427180
Muhammad Haris Khan <developewithharis@gmail.com>
2022-07-22 21:50:57 +0500

    Third commit

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
-const list = document.querySelector(".grocery-list");
-const clearBtn = document.querySelector(".clear-btn");
-let editElement;
-let editFlag = false;
-let edited = "";
-FormData.addEventListner("submit", addItem);
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  formValidation();
+});
+
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Input cannot be blank";
+    console.log("failure");
+  } else {
+    console.log("successs");
+    msg.innerHTML = "";
+    acceptData();
+  }
+};
+
+let data = {};
+
+let acceptData = () => {
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
+};
+
+let createPost = () => {
+  posts.innerHTML += `
+  <div>
+    <p>${data.text} 
+      <i onClick="deletePost(this)" class="bi bi-trash"></i>
+    </p>
+  </div>
+  `;
+  input.value = "";
+};
+
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
